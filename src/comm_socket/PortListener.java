472d12092647c86b4dc396f9e3cf34fa5a5b81a6
Ibrahim Yener <yener@ibrahim-yener.com>
2016-06-16 23:00:46 +0900

    Runnable arayuzu ilave edildi

--- src/comm_socket/PortListener.java
+++ src/comm_socket/PortListener.java
@@ -1,9 +1,16 @@
 package comm_socket;
 
-public class PortListener
+public class PortListener implements Runnable
 {
 	public static void listenMyPort()
 	{
+		
+	}
+
+	@Override
+	public void run()
+	{
+		// This Class will run 7/24 hour listen for incoming
 		
 	}
 }
