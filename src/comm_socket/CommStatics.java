9732785495bbf73f3219a04ddcde313a4069764a
Ibrahim Yener <yener@ibrahim-yener.com>
2016-06-16 22:58:03 +0900

    Comm Socket listener sablonu

---
+++ src/comm_socket/CommStatics.java
@@ -0,0 +1,7 @@
+package comm_socket;
+
+public class CommStatics
+{
+	public static int DATABITS_5 = 5;
+	
+}
--- src/comm_socket/PortListener.java
+++ src/comm_socket/PortListener.java
@@ -2,5 +2,8 @@ package comm_socket;
 
 public class PortListener
 {
-
+	public static void listenMyPort()
+	{
+		
+	}
 }
